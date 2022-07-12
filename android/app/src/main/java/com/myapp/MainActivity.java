c48d664044e84e1fc45453743fccf578d5b0ec05
Allan Ramos <allangrds@gmail.com>
2022-07-11 23:32:52 -0300

    feat(project): changing app name

--- android/app/src/main/java/com/myapp/MainActivity.java
+++ android/app/src/main/java/com/myapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "MyApp";
+    return "Nisto Cremos";
   }
 
   @Override
