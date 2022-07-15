ecf99de9fc44a139d32e89147c0d544bb0f77177
Allan Gabriel Ramos da Silva <allan.silva@rain.us>
2022-07-15 19:08:40 -0300

    fix(detail): add box with background-color

--- src/screen/detail/detail.tsx
+++ src/screen/detail/detail.tsx
@@ -18,90 +18,93 @@ export const Detail = ({ route }: any) => {
 
   return (
     <>
-      <Header withHeaderNavigation />
-      {
-        isLoading
-          ? <Loading />
-          : (
-            <Box paddingX={4} paddingBottom={4}>
-              <FlatList
-                ListHeaderComponent={(
-                  <>
-                    <Heading
-                      size="md"
-                      marginBottom={2}
-                      marginTop={4}
-                      color={useColorModeValue('black', 'white')}
-                    >
-                      {data.name}
-                    </Heading>
-                    <Text
-                      lineHeight={28}
-                      fontSize="md"
-                      color={useColorModeValue('black', 'white')}
-                    >
-                      {data.text}
-                    </Text>
-                    <HStack
-                      marginBottom={4}
-                      marginTop={2}
-                      flexDirection="row"
-                      flexWrap="wrap"
-                      space={2}
-                    >
+      <Box backgroundColor={useColorModeValue('white', 'muted.900')}>
+        <Header withHeaderNavigation />
+        {
+          isLoading
+            ? <Loading />
+            : (
+              <Box paddingX={4} paddingBottom={4}>
+                <FlatList
+                  ListFooterComponent={<Box marginBottom={180} />}
+                  ListHeaderComponent={(
+                    <>
+                      <Heading
+                        size="md"
+                        marginBottom={2}
+                        marginTop={4}
+                        color={useColorModeValue('black', 'white')}
+                      >
+                        {data.name}
+                      </Heading>
+                      <Text
+                        lineHeight={28}
+                        fontSize="md"
+                        color={useColorModeValue('black', 'white')}
+                      >
+                        {data.text}
+                      </Text>
+                      <HStack
+                        marginBottom={4}
+                        marginTop={2}
+                        flexDirection="row"
+                        flexWrap="wrap"
+                        space={2}
+                      >
+                        {
+                          data.biblical_passages && data.biblical_passages.length > 0
+                            ? (
+                              data.biblical_passages.map(biblicalPassage => (
+                                <Text
+                                  key={biblicalPassage.label}
+                                  fontSize="lg"
+                                  fontWeight="bold"
+                                >
+                                  {biblicalPassage.label}
+                                </Text>
+                              ))
+                            ) : null
+                        }
+                      </HStack>
                       {
-                        data.biblical_passages && data.biblical_passages.length > 0
+                        data.youtubeIds && data.youtubeIds.length > 0
                           ? (
-                            data.biblical_passages.map(biblicalPassage => (
-                              <Text
-                                key={biblicalPassage.label}
-                                fontSize="lg"
-                                fontWeight="bold"
-                              >
-                                {biblicalPassage.label}
-                              </Text>
+                            data.youtubeIds.map((videoId: string) => (
+                              <YoutubePlayer
+                                key={videoId}
+                                height={213}
+                                videoId={videoId}
+                              />
                             ))
                           ) : null
                       }
-                    </HStack>
-                    {
-                      data.youtubeIds && data.youtubeIds.length > 0
-                        ? (
-                          data.youtubeIds.map((videoId: string) => (
-                            <YoutubePlayer
-                              key={videoId}
-                              height={213}
-                              videoId={videoId}
-                            />
-                          ))
-                        ) : null
-                    }
-                    <Heading
-                      size="md"
-                      marginBottom={2}
-                      marginTop={4}
-                      color={useColorModeValue('black', 'white')}
-                    >
-                      Passagens
-                    </Heading>
-                  </>
-                )}
-                data={data.biblical_passages}
-                keyExtractor={item => item.label}
-                renderItem={({ item }) => (
-                  <Box marginBottom={3}>
-                    <Text fontSize="lg" fontWeight="bold">
-                      {item.label}
-                    </Text>
-                    <Text fontSize="md">
-                      {item.passage}
-                    </Text>
-                  </Box>
-                )}
-              />
-            </Box>
-          )
-      }
+                      <Heading
+                        size="md"
+                        marginBottom={2}
+                        marginTop={4}
+                        color={useColorModeValue('black', 'white')}
+                      >
+                        Passagens
+                      </Heading>
+                    </>
+                  )}
+                  data={data.biblical_passages}
+                  keyExtractor={item => item.label}
+                  renderItem={({ item }) => (
+                    <Box marginBottom={3}>
+                      <Text fontSize="lg" fontWeight="bold">
+                        {item.label}
+                      </Text>
+                      <Text fontSize="md">
+                        {item.passage}
+                      </Text>
+                    </Box>
+                  )}
+                />
+              </Box>
+            )
+        }
+      </Box>
     </>
   )
 }
